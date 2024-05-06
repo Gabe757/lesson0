5699ab70d77854427b939df0ad41362ac6644c3e
Gabe757 <gabrieltionsha7@gmail.com>
2024-05-06 05:35:02 -0700

    Created profile_button function

--- hello_world.py
+++ hello_world.py
@@ -2,3 +2,7 @@ Print ("hello world")
 #My second print statement 
 print("My second hello World")
 
+def profile_button():
+    print("I will update user profile when clicked")
+
+profile_button()
---
+++ hollo_world.py
@@ -0,0 +1,4 @@
+def profile_button():
+    print("I will update user profile when clicked")
+
+    profile_button()
